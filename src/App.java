f4938781156fd541da9160340f65c0cab87e0369
hyunmang2 <166034905+hyunmang2@users.noreply.github.com>
2024-04-30 00:42:02 +0900

    1-2번 끝!

--- src/App.java
+++ src/App.java
@@ -1,13 +1,16 @@
 import java.util.Scanner;
 
 
-public class Main {
+public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("첫 번째 숫자를 입력하세요: ");
-        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+        int num1 = sc.nextInt(); // 첫 번째 정수를 전달 받아 num1 변수에 저장
         System.out.print("두 번째 숫자를 입력하세요: ");
-        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다
+        int num2 = sc.nextInt(); // 두 번째 정수를 전달 받아 num2 변수에 저장
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        char forchik = sc.next().charAt(0);
+
     }
 }
