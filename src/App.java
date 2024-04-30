8e9c236ceba18371b17662a12a60acd28a4d232f
hyunmang2 <166034905+hyunmang2@users.noreply.github.com>
2024-04-30 14:45:43 +0900

    1-6번 끝!

--- src/App.java
+++ src/App.java
@@ -1,5 +1,4 @@
-import java.util.Objects;
-import java.util.Scanner;
+import java.util.*;
 
 
 public class App {
@@ -7,7 +6,7 @@ public class App {
         Scanner sc = new Scanner(System.in);
 
         int result = 0; // 변수값을 초기화
-        double[] doubleArray = new double[10]; // while 돌고나서 저장되는 값이 인덱스 0번째에만 저장이 되니까
+        double[] doubleArray = new double[9]; // while 돌고나서 저장되는 값이 인덱스 0번째에만 저장이 되니까
         int index = 0;
 
         while (true) {
@@ -33,18 +32,32 @@ public class App {
                     result = num1 * num2;
                     break;
             }
-
-            doubleArray[index] = result; // index번째 있는 더블어레이에 결과값을 저장한다.
+//            ArrayList<Integer> intList = new ArrayList<>(); //생성자
+           doubleArray[index] = result; // index번째 있는 더블어레이에 결과값을 저장한다.
             index = index + 1; // index 증가
+//           intList.add(result); // 저장된 result값 받기!
+            if (index > doubleArray.length-1) { // 인덱스는 0~9번까진데 배열 9번째칸 보다 인덱스가 크면
+                double[] newArray = new double[10]; // 1칸씩 땡긴 값을 저장하는 배열 생성
+                for (int i = 0; i < 9; i++) { // 1칸씩 이동하면서 저장? 10칸에서 한칸씩 9번땡기기
+                    newArray[i] = doubleArray[i + 1]; // i번째껀 삭제 되니까
+                }
+                newArray[9] = result; // 인덱스 9번째 = 배열 10번칸에 새로운 결과 값을 저장
+                doubleArray = newArray; // 더블어레이에 있는 값을 쓰고싶다 = 대입!
+            }
 
             System.out.println("결과: " + result);
+//            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+//            sc.nextLine(); // remove 입력 전 엔터치고 넘어 가려고
+//            String input = sc.nextLine(); // remove를 입력하기 위한 입력값 저장
+//
+//            if (input.equals("remove")) { // 만약에~ input으로 입력한 것이 remove가 맞으면 실행해라~
+//                intList.remove(0); // 0번째 index 삭제!
+//            }
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-            sc.nextLine();
             String text = sc.nextLine();
             if (text.equals("exit")) {
                 return;
             }
-
         }
     }
 }
